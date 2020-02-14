3f9dc15b38d2cc05082613a9a9b72c2c3e2c798c
Piotr Marczuk <pmz@kmd.dk>
2020-02-14 14:13:14 +0100

    GUI for login page

--- foot-track/src/app/app.module.ts
+++ foot-track/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login/login-page/login-page.component';
+import { LoginFormComponent } from './login/login-form/login-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ foot-track/src/app/login/login-form/login-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.less']
+})
+export class LoginFormComponent {
+
+  constructor() { }
+
+  onSubmit(form: NgForm) {
+  }
+
+}
