0ae7435827a06ca9e0700e6a99671ef5d6ad2e7b
Piotr Marczuk <pmz@kmd.dk>
2020-02-17 21:01:00 +0100

    Reroute after logout

--- foot-track/src/app/home/home.component.ts
+++ foot-track/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../_services/authentication.service';
 import { User } from '../_models/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,7 @@ import { User } from '../_models/user.model';
 export class HomeComponent implements OnInit {
 
   currentUser: User;
-  constructor(private authenticationService: AuthenticationService) {
+  constructor(private authenticationService: AuthenticationService, private router: Router) {
   }
 
   ngOnInit(): void {
@@ -19,6 +20,7 @@ export class HomeComponent implements OnInit {
 
   logout() {
     this.authenticationService.logout();
+    this.router.navigate(['/login']);
   }
 
 }
