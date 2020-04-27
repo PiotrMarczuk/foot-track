5f504d146935e4b4157e3279d4cc6e9ea3371783
Piotr Marczuk <01133465@pw.edu.pl>
2020-04-27 23:30:45 +0200

    Updated imports

--- src/FootTrack.Web/src/app/shared/shared.module.ts
+++ src/FootTrack.Web/src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
