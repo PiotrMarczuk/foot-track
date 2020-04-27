e334183e63f34141b899b8f5de33ee835c08f0ca
Piotr Marczuk <01133465@pw.edu.pl>
2020-04-27 21:02:47 +0200

    Deleted username from user model

--- src/FootTrack.Web/src/app/core/services/authentication.service.ts
+++ src/FootTrack.Web/src/app/core/services/authentication.service.ts
@@ -21,8 +21,8 @@ export class AuthenticationService {
     return this.currentUserSubject.value;
   }
 
-  login(username: string, password: string) {
-    return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { username, password })
+  login(email: string, password: string) {
+    return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { email, password })
       .pipe(map(user => {
         localStorage.setItem('currentUser', JSON.stringify(user));
         this.currentUserSubject.next(user);
