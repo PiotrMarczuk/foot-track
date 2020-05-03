4b2024828a68ab09ad42f379dc06249400b28878
Piotr Marczuk <01133465@pw.edu.pl>
2020-05-03 20:27:49 +0200

    Fixed modules

--- src/FootTrack.Web/src/app/app.module.ts
+++ src/FootTrack.Web/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, NgbModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    NgbModule,
+    SharedModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/FootTrack.Web/src/app/shared/shared.module.ts
+++ src/FootTrack.Web/src/app/shared/shared.module.ts
@@ -7,5 +7,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [FooterComponent, NavbarComponent],
   imports: [CommonModule, NgbModule],
+  exports: [FooterComponent, NavbarComponent]
 })
 export class SharedModule {}
