9c63f82537f9eb9ffab95052a8807c98fb3a0a8c
Piotr Marczuk <01133465@pw.edu.pl>
2020-05-03 20:54:28 +0200

    Improved navbar - moved router to view

--- src/FootTrack.Web/src/app/shared/components/navbar/navbar.component.ts
+++ src/FootTrack.Web/src/app/shared/components/navbar/navbar.component.ts
@@ -25,7 +25,6 @@ export class NavbarComponent implements OnInit, OnDestroy {
 
   logout() {
     this.authenticationService.logout();
-    this.router.navigate(['/identify/login']);
   }
 
   ngOnInit(): void {
