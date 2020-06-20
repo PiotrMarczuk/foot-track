e7757c28936c6de81e01c1bf7b14169da3a159fb
Piotr Marczuk <pmz@kmd.dk>
2020-06-20 15:31:43 +0200

    Added abstraction layer for entities in Mongo

---
+++ src/FootTrack.Api/FootTrack.Api/Models/Document/Document.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using MongoDB.Bson;
+
+namespace FootTrack.Api.Models.Document
+{
+    public abstract class Document : IDocument
+    {
+        public ObjectId Id { get; set; }
+
+        public DateTime CreatedAt => Id.CreationTime;
+    }
+}
