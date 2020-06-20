0353ddac13e515d68192b41e774e003524b21027
Piotr Marczuk <pmz@kmd.dk>
2020-06-21 00:02:07 +0200

    Added custom exceptions for better error handling purposes

---
+++ src/FootTrack.Api/FootTrack.Api/Exceptions/NotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FootTrack.Api.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() : this("Provided object already exist.")
+        {
+
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
