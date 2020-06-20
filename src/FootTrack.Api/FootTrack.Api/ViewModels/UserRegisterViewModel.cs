19b7aec5ae3f893e9f30c2f6e71e0a59d4639881
Piotr Marczuk <pmz@kmd.dk>
2020-06-20 22:11:28 +0200

    Added ViewModels for user

---
+++ src/FootTrack.Api/FootTrack.Api/ViewModels/UserRegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FootTrack.Api.ViewModels
+{
+    public class UserRegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [MinLength(6, ErrorMessage = "Password length should be longer than 6")]
+        [Required]
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+    }
+}
