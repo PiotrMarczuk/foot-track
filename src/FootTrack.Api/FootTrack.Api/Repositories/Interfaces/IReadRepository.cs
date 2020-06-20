b1f1b15ffcb35af035e39a8cd09c693752353933
Piotr Marczuk <pmz@kmd.dk>
2020-06-20 15:52:05 +0200

    Added interfaces for repositories

---
+++ src/FootTrack.Api/FootTrack.Api/Repositories/Interfaces/IReadRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+using FootTrack.Api.Models.Document;
+
+namespace FootTrack.Api.Repositories.Interfaces
+{
+    public interface IReadRepository<TDocument> where TDocument : IDocument
+    {
+        IEnumerable<TDocument> FilterBy(
+            Expression<Func<TDocument, bool>> filterExpression);
+
+        IEnumerable<TProjected> FilterBy<TProjected>(
+            Expression<Func<TDocument, bool>> filterExpression,
+            Expression<Func<TDocument, TProjected>> projectionExpression);
+
+        TDocument FindOne(Expression<Func<TDocument, bool>> filterExpression);
+
+        Task<TDocument> FindOneAsync(Expression<Func<TDocument, bool>> filterExpression);
+
+        TDocument FindById(string id);
+
+        Task<TDocument> FindByIdAsync(string id);
+    }
+}
---
+++ src/FootTrack.Api/FootTrack.Api/Repositories/Interfaces/IUpdateRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+using FootTrack.Api.Models.Document;
+
+namespace FootTrack.Api.Repositories.Interfaces
+{
+    public interface IUpdateRepository<in TDocument> where TDocument : IDocument
+    {
+        void ReplaceOne(TDocument document);
+
+        Task ReplaceOneAsync(TDocument document);
+    }
+}
