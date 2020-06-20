605e192a006655dfca494da059e4a960d2e05b37
Piotr Marczuk <pmz@kmd.dk>
2020-06-20 15:40:01 +0200

    Added User document

---
+++ src/FootTrack.Api/FootTrack.Api/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using FootTrack.Api.Attributes;
+
+namespace FootTrack.Api.Models
+{
+    [BsonCollection("users")]
+    public class User : Document.Document
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+    }
+}
