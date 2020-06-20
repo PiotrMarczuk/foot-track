a44ec95393ecf99f0cb04947e69643f1bba37a4a
Piotr Marczuk <pmz@kmd.dk>
2020-06-20 15:34:02 +0200

    Added attribute for naming documents

---
+++ src/FootTrack.Api/FootTrack.Api/Attributes/BsonCollectionAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FootTrack.Api.Attributes
+{
+    public class BsonCollectionAttribute : Attribute
+    {
+        public string CollectionName { get; }
+
+        public BsonCollectionAttribute(string collectionName)
+        {
+            CollectionName = collectionName;
+        }
+    }
+}
