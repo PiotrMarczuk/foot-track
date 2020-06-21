5c1f47ce9e17b4f579c2ac35e7a19ac3e39e8490
Piotr Marczuk <pmz@kmd.dk>
2020-06-21 12:21:35 +0200

    Added attribute for model validation

---
+++ src/FootTrack.Api/FootTrack.Api/Attributes/ModelValidationFilterAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FootTrack.Api.Attributes
+{
+    public class ModelValidationFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ModelState.IsValid) return;
+
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            context.Result = new BadRequestObjectResult(context.ModelState);
+        }
+    }
+}
