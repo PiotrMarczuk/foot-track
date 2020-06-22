999a7efa82e9cba9798ad28d6bce1812d69c9146
Piotr Marczuk <PMZ@kmd.dk>
2020-06-22 21:49:16 +0200

    Added swagger documentation in user controller

--- src/FootTrack.Api/FootTrack.Api/Controllers/V1/UsersController.cs
+++ src/FootTrack.Api/FootTrack.Api/Controllers/V1/UsersController.cs
@@ -2,6 +2,7 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 using FootTrack.Api.Attributes;
 using FootTrack.Api.Contracts.V1;
@@ -10,6 +11,9 @@ using FootTrack.Api.ViewModels;
 
 namespace FootTrack.Api.Controllers.V1
 {
+    /// <summary>
+    /// Operations about user.
+    /// </summary>
     [ApiController]
     [Authorize]
     public class UsersController : ControllerBase
@@ -25,8 +29,18 @@ namespace FootTrack.Api.Controllers.V1
             _userService = userService;
         }
 
+        /// <summary>
+        /// Logs in a user.
+        /// </summary>
+        /// <param name="loginViewModel">Model with email and password.</param>
+        /// <returns>A logged in user with token.</returns>
+        /// <response code="200">Returns logged in user with token.</response>
+        /// <response code="400">If loginViewModel is not valid.</response>
+        /// <response code="401">When credentials are wrong.</response>
         [AllowAnonymous]
         [ServiceFilter(typeof(ModelValidationFilterAttribute))]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPost(ApiRoutes.Users.Login)]
         public async Task<IActionResult> Login([FromBody] UserLoginViewModel loginViewModel)
         {
@@ -34,8 +48,19 @@ namespace FootTrack.Api.Controllers.V1
                 .AuthenticateAsync(loginViewModel));
         }
 
+        /// <summary>
+        /// Registers a user.
+        /// </summary>
+        /// <param name="registerViewModel">Model for creating new user.</param>
+        /// <returns>Newly created user.</returns>
+        /// <response code="201">Returns newly created user.</response>
+        /// <response code="400">If registerViewModel is not valid.</response>
+        /// <response code="409">If user with provided email already exists.</response>
         [AllowAnonymous]
         [ServiceFilter(typeof(ModelValidationFilterAttribute))]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
         [HttpPost(ApiRoutes.Users.Register)]
         public async Task<IActionResult> Register([FromBody] UserRegisterViewModel registerViewModel)
         {
@@ -48,7 +73,20 @@ namespace FootTrack.Api.Controllers.V1
                 _mapper.Map<UserViewModel>(user));
         }
 
+        /// <summary>
+        /// Method for getting user data by id.
+        /// </summary>
+        /// <param name="id">User id.</param>
+        /// <returns>User data.</returns>
+        /// <response code="200">Returns user data.</response>
+        /// <response code="400">If provided id is not valid.</response>
+        /// <response code="401">If there was no auth token provided.</response>
+        /// <response code="404">If user with provided id was not found.</response>
         [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet(ApiRoutes.Users.GetById)]
         [Cached(300)]
         public async Task<IActionResult> GetById(string id)
