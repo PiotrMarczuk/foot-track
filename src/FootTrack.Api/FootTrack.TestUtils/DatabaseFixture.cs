64b7ca95b87b724d18324f64a8f1123c638717df
Piotr Marczuk <PMZ@kmd.dk>
2020-09-07 19:52:42 +0200

    Optimization towards repository integration tests

--- src/FootTrack.Api/FootTrack.TestUtils/DatabaseFixture.cs
+++ src/FootTrack.Api/FootTrack.TestUtils/DatabaseFixture.cs
@@ -8,26 +8,29 @@ namespace FootTrack.TestUtils
 {
     public class DatabaseFixture : IDisposable
     {
+        private readonly string _originalDbName;
         private readonly IMongoDbSettings _dbSettings;
         private readonly IMongoClient _mongoClient;
-
-        public IMongoDatabase MongoDb { get; }
 
         public DatabaseFixture()
         {
             IConfigurationRoot configRoot = new ConfigurationBuilder().AddJsonFile("appsettings.test.json").Build();
 
-            string originalDbName = configRoot.GetSection(nameof(MongoDbSettings))["DatabaseName"];
-            var dbName = $"test_db_{originalDbName}_{Guid.NewGuid()}";
+            _originalDbName = configRoot.GetSection(nameof(MongoDbSettings))["DatabaseName"];
 
             _dbSettings = new MongoDbSettings
             {
                 ConnectionString = configRoot.GetSection(nameof(MongoDbSettings))["ConnectionString"],
-                DatabaseName = dbName,
             };
 
             _mongoClient = new MongoClient(_dbSettings.ConnectionString);
-            MongoDb = new MongoDatabaseFactory(_dbSettings).Create();
+        }
+
+        public IMongoDatabase CreateMongoDatabase()
+        {
+            _dbSettings.DatabaseName = $"test_db_{_originalDbName}_{Guid.NewGuid()}";
+
+            return new MongoDatabaseFactory(_dbSettings).Create();
         }
 
         public void Dispose()
