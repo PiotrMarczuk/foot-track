ec657d6c496b2648153c4a278759ad74b0663833
Piotr Marczuk <PMZ@kmd.dk>
2020-10-11 13:28:42 +0200

    Live messaging working from RPI

--- src/FootTrack.Web/src/components/GoogleHeatmap.vue
+++ src/FootTrack.Web/src/components/GoogleHeatmap.vue
@@ -5,21 +5,24 @@
 <script lang="ts">
 import { Loader, LoaderOptions } from "google-maps";
 import { Component, Prop, Vue } from "vue-property-decorator";
+import { HubConnectionBuilder, LogLevel } from "@aspnet/signalr";
 
 @Component
 export default class GoogleHeatmap extends Vue {
   points: Array<google.maps.LatLng> = new Array<google.maps.LatLng>();
-  map: any;
-  heatmap: any;
+  map!: google.maps.Map;
+  heatmap!: google.maps.visualization.HeatmapLayer;
 
   async mounted() {
     const loader = new Loader(process.env.VUE_APP_GOOGLE_API_KEY, {
       libraries: ["visualization"]
     });
     const google = await loader.load();
+    const initialMapCenter = new google.maps.LatLng(52.2663005, 21.0320765);
+
     this.map = new google.maps.Map(document.getElementById("map")!, {
-      center: { lat: 37.782, lng: -122.447 },
-      zoom: 20
+      center: initialMapCenter,
+      zoom: 18
     });
 
     this.heatmap = new google.maps.visualization.HeatmapLayer({
@@ -27,12 +30,26 @@ export default class GoogleHeatmap extends Vue {
     });
 
     setInterval(this.reloadHeatmap, 10000);
+    const connection = new HubConnectionBuilder()
+      .withUrl(process.env.VUE_APP_HUBURL || "")
+      .configureLogging(LogLevel.Information)
+      .build();
+
+    connection.on("TrainingMessage", data => {
+      const newPoint = new google.maps.LatLng(data.latitude, data.longitude);
+      this.points.push(newPoint);
+      this.map.setCenter(newPoint);
+    });
+
+    connection.start();
   }
 
   private reloadHeatmap() {
     this.heatmap.setMap(null);
     this.heatmap.setMap(this.map);
-    console.log('reloaded');
+    if (this.points.length > 0) {
+      this.map.setCenter(this.points[this.points.length - 1]);
+    }
   }
 }
 </script>
